03cd325f532616efb05b3a881736289980ed31d5
ZahidMC <20233l001139@utcv.edu.mx>
2026-03-09 17:02:31 -0600

    LandingPageHTAS2 V5

--- src/app/template/footer/footer.ts
+++ src/app/template/footer/footer.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { RouterLink, Router } from "@angular/router";
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,27 @@ import { RouterLink } from "@angular/router";
   styleUrl: './footer.css',
 })
 export class Footer {
+  private isBrowser: boolean;
 
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private router: Router
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  scrollToFragment(fragment: string): void {
+    if (this.router.url.startsWith('/landing')) {
+      if (fragment === 'inicio') {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      } else {
+        const element = document.getElementById(fragment);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+      }
+    } else {
+      this.router.navigate(['/landing'], { fragment: fragment });
+    }
+  }
 }
