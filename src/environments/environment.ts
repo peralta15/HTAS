2749eb51bb38828f52f1f1dd6bd71c46d1a8b482
Zahid-Monraga-Contreras <20233L001139@utcv.edu.mx>
2026-06-08 11:01:02 -0600

    Web a la nube

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,17 @@
 export const environment = {
-    production: false,
-    baseUrl: 'http://localhost:3000',
-    // Ruta específica para autenticación (Register/Login)
-    authApi: 'http://localhost:3000/api/auth',
-    // Ruta para gestión de usuarios (CRUD)
-    usersApi: 'http://localhost:3000/api/users',
+    production: true,
+    baseUrl: 'https://htas-backend-node.onrender.com',
 
-    iaApi: 'http://localhost:3000/api/ia',
-    htasApi: 'http://localhost:3000/api/htas',
+    // Ruta específica para autenticación (Register/Login)
+    authApi: 'https://htas-backend-node.onrender.com/api/auth',
+
+    // Ruta para gestión de usuarios (CRUD)
+    usersApi: 'https://htas-backend-node.onrender.com/api/users',
+
+    iaApi: 'https://htas-backend-node.onrender.com/api/ia',
+    htasApi: 'https://htas-backend-node.onrender.com/api/htas',
 
     stripePublicKey: 'pk_test_51TF59VK1qKH77YTduyJn44BruhEDaWGDisu6ry0DtNKZwiTnFGyMysighKG4gecGIdX4TSFc66sike4gZoJ1xCXA008uHnREBG',
-    checkoutApi: 'http://localhost:3000/api/auth/create-checkout-session',
+    checkoutApi: 'https://htas-backend-node.onrender.com/api/auth/create-checkout-session',
     recaptchaSiteKey: '6LdV0u8sAAAAAGK4Rkxxa98_h0eb2lUng2dv7RZa'
 };
