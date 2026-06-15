c07c323bc2d20b7c057f1bb945bf0cea17259fa0
Zahid-Monraga-Contreras <20233L001139@utcv.edu.mx>
2026-06-14 20:02:15 -0600

    Web a la nube V2

--- src/db/database.js
+++ src/db/database.js
@@ -6,10 +6,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: 5432,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  port: process.env.DB_PORT,
 });
 
 module.exports = {
